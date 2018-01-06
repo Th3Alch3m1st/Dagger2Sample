b88905071b191b0d08fe0e191e590e0299969c97
Th3Alch3m1st <rony.icpc@gmail.com>
2018-01-07 00:56:44 +0600

    Initial commit

---
+++ app/src/main/java/com/palidrome/dagger2test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.palidrome.dagger2test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    int x =0 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
